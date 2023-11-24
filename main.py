7b5bf6adf1b68c77a97b3b8e91552a01f6005cb5
implaymo <136201112+implaymo@users.noreply.github.com>
2023-11-24 17:57:03 +0000

    Finished

--- main.py
+++ main.py
@@ -16,8 +16,10 @@ chrome_options.add_experimental_option("detach", value=True)
 
 # Open website
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://www.linkedin.com/jobs/search/?currentJobId=3766025974&f_AL=true&f_E="
-           "2&keywords=remote%20job%20search&origin=JOB_SEARCH_PAGE_JOB_FILTER")
+driver.get("https://www.linkedin.com/jobs/search/?currentJobId=3754274123&f_AL="
+           "true&f_E=2&keywords=remote%20job%20search&origin=JOB_SEARCH_PAGE_JOB_FILTER")
+
+driver.maximize_window()
 
 sign_in = driver.find_element(By.XPATH, '/html/body/div[3]/header/nav/div/a[2]')
 sign_in.click()
@@ -32,25 +34,44 @@ sign_in_button = driver.find_element(By.XPATH, '//*[@id="organic-div"]/form/div[
 sign_in_button.click()
 
 
+time.sleep(5)
 
-## NOT WORKING, NEEDS FIXING
-try:
-    minimize_chat_button = WebDriverWait(driver, 10).until(
-        EC.visibility_of_element_located((By.XPATH, '//*[@id="ember42"]/svg/use'))
-    )
-    minimize_chat_button.click()
+def save_job():
+    """Clicks SAVE to save the job"""
+    try:
+        save_button = WebDriverWait(driver, 5).until(
+            EC.visibility_of_element_located((By.XPATH, '//*[@id="main"]/div/div[2]/div/div[2]/div/div[1]/div/div[1]/div/div[1]/div[1]/div[4]/div/button'))
+        )
+        save_button.click()
 
-    print("Actions performed successfully!")
-except TimeoutError:
-    print("Timed out waiting for the element to be visible.")
-except Exception as e:
-    print(f"An error occurred: {e}")
-finally:
-    driver.quit()
-
+        print("Actions performed successfully!")
+    except TimeoutError:
+        print("Timed out waiting for the element to be visible.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 
-    # easy_apply = WebDriverWait(driver, 20).until(
-    #     EC.visibility_of_element_located((By.XPATH, '//*[@id="ember228"]/span'))
-    # )
-    # easy_apply.click()
+# Find all jobs
+jobs = WebDriverWait(driver, 10).until(
+    EC.visibility_of_any_elements_located((By.CLASS_NAME, 'disabled.ember-view.job-card-container__link.job-card-list__title')))
+
+for i in range(len(jobs)):
+    try:
+        # Re-locate the jobs after each iteration
+        jobs = WebDriverWait(driver, 10).until(
+            EC.visibility_of_any_elements_located((By.CLASS_NAME, 'disabled.ember-view.job-card-container__link.job-card-list__title'))
+        )
+
+        print(jobs[i].text)
+        jobs[i].click()
+
+        # Introduce a delay or use WebDriverWait for the job details to load
+        time.sleep(2)  # You can adjust the sleep time based on your observation
+
+        save_job()
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        driver.quit()
+
